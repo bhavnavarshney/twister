1dc4a5f9caa80b8b14942916417fa3eecd80eebb
Jack Zheng <jackz@student.unimelb.edu.au>
2020-06-14 22:33:18 +1000

    Add message details and structure

--- backend/pkg/message/message.go
+++ backend/pkg/message/message.go
@@ -2,12 +2,35 @@
 
 package message
 
+const (
+	drillType    = 0x04
+	drillID      = 0x05
+	singleParam  = 0x06
+	torqueOffset = 0x11
+	readOffset   = 0x13
+	bulkParam    = 0x14
+)
+
+type DrillType struct {
+	header   byte
+	dataInfo byte
+	message  [8]byte
+	checksum []byte
+}
+
 type DrillID struct {
 	header   byte
-	message  []byte
+	dataInfo byte
+	message  [6]byte
 	checksum []byte
 }
 
 func (id *DrillID) Marshal() []byte {
 	return append([]byte{id.header}, id.message...)
+}
+
+type Command struct {
+	command byte
+	retries int
+	timeout int
 }
