4249398673c40ebc4133dd42faa8e36f95b45153
Jack Zheng <jackz@student.unimelb.edu.au>
2020-06-15 21:42:35 +1000

    Extend to handle slices of nibbles

--- backend/pkg/nibble/nibble.go
+++ backend/pkg/nibble/nibble.go
@@ -1,6 +1,26 @@
 package nibble
 
 // Takes a byte and breaks the bits into two nibbles
+func BreakSlice(input []byte) []byte {
+	var result []byte
+	for i := range input {
+		high, low := Break(input[i])
+		result = append(result, high, low)
+	}
+	return result
+}
+
+// Takes a byte and breaks the bits into two nibbles
+func AssembleSlice(input []byte) []byte {
+	var result []byte
+	for i := 0; i < len(input); i = i + 2 {
+		assembled := Assemble(input[i], input[i+1])
+		result = append(result, assembled)
+	}
+	return result
+}
+
+// Takes a byte and breaks the bits into two nibbles
 func Break(input byte) (byte, byte) {
 	return input >> 4, (input & 0x0F)
 }
--- backend/pkg/nibble/nibble_test.go
+++ backend/pkg/nibble/nibble_test.go
@@ -16,3 +16,15 @@ func TestAssemble(t *testing.T) {
 	assembled := Assemble(byte(0x0A), byte(0x0F))
 	assert.Equal(t, byte(0xAF), assembled)
 }
+
+func TestAssembleSlice(t *testing.T) {
+	input := []byte{0x0F, 0x0F, 0x03, 0x0A, 0x00, 0x01}
+	expected := []byte{0xFF, 0x3A, 0x01}
+	assert.Equal(t, expected, AssembleSlice(input))
+}
+
+func TestBreakSlice(t *testing.T) {
+	input := []byte{0xFF, 0x3A, 0x01}
+	expected := []byte{0x0F, 0x0F, 0x03, 0x0A, 0x00, 0x01}
+	assert.Equal(t, expected, BreakSlice(input))
+}
