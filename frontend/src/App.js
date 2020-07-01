f51658415147f19e1121a0f3a082bd3268ecec34
Unknown <jackz@student.unimelb.edu.au>
2020-07-01 21:59:57 +1000

    Replace material snackbar
    Previously the snackbar caused the table to drop out of edit mode. Easiest option seemed to be to change the snackbar library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { SnackbarProvider } from 'notistack';
 import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
+    <SnackbarProvider>
     <div id="app" className="App">
        <HelloWorld />
     </div>
+    </SnackbarProvider>
   );
 }
 
