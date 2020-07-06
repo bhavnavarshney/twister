ecd7b6fba30b357f445ff344b430f67d277b2e82
Jack Zheng <j.zheng@live.com.au>
2020-07-06 21:07:50 +1000

    Resize table

--- frontend/src/components/HelloWorld.js
+++ frontend/src/components/HelloWorld.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react";
-import { useSnackbar } from 'notistack';
+import React, { useEffect } from "react";
+import { useSnackbar } from "notistack";
 import Grid from "@material-ui/core/Grid";
 import InfoCard from "./InfoCard";
 import ParamTable from "./ParamTable";
-import Wails from "@wailsapp/runtime"
+import Wails from "@wailsapp/runtime";
 
 function mapFieldsToProfile(fields) {
   return fields.map((item, index) => {
@@ -29,69 +29,83 @@ export default function HelloWorld() {
   const [port, setPort] = React.useState(3);
   const [profile, setProfile] = React.useState([]);
   const [isConnected, setIsConnected] = React.useState(false);
-  const { enqueueSnackbar } = useSnackbar()
+  const { enqueueSnackbar } = useSnackbar();
 
-  const infoSnackBarOptions = {variant: "info", autoHideDuration:3000, anchorOrigin:{
-    vertical: "bottom",
-    horizontal: "right",
-  }}
-  
-  const errorSnackBarOptions = {variant: "error", anchorOrigin:{
-    vertical: "bottom",
-    horizontal: "right",
-  }}
+  const infoSnackBarOptions = {
+    variant: "info",
+    autoHideDuration: 3000,
+    anchorOrigin: {
+      vertical: "bottom",
+      horizontal: "right",
+    },
+  };
 
-  const successSnackBarOptions = {variant: "success", autoHideDuration:3000, anchorOrigin:{
-    vertical: "bottom",
-    horizontal: "right",
-  }}
+  const errorSnackBarOptions = {
+    variant: "error",
+    anchorOrigin: {
+      vertical: "bottom",
+      horizontal: "right",
+    },
+  };
+
+  const successSnackBarOptions = {
+    variant: "success",
+    autoHideDuration: 3000,
+    anchorOrigin: {
+      vertical: "bottom",
+      horizontal: "right",
+    },
+  };
 
   useEffect(() => {
-    Wails.Events.On("CurrentOffset", message => {
-      setCurrentOffset(message)
+    Wails.Events.On("CurrentOffset", (message) => {
+      setCurrentOffset(message);
     });
   }, []);
 
-
   const handleClose = () => {
-    window.backend.Drill.Close().then((result) => {
-      setCurrentOffset(null)
-      setInfo({})
-      setProfile([])
-      enqueueSnackbar("Closed", infoSnackBarOptions)
-      setIsConnected(false)
-    }).catch((err)=> {
-      enqueueSnackbar("Error Closing port:" + err, infoSnackBarOptions)
-      setIsConnected(false)
-    });
-  }
+    window.backend.Drill.Close()
+      .then((result) => {
+        setCurrentOffset(null);
+        setInfo({});
+        setProfile([]);
+        enqueueSnackbar("Closed", infoSnackBarOptions);
+        setIsConnected(false);
+      })
+      .catch((err) => {
+        enqueueSnackbar("Error Closing port:" + err, infoSnackBarOptions);
+        setIsConnected(false);
+      });
+  };
 
   const handleSetPort = (e) => {
-    setPort(e.target.value)
-  }
+    setPort(e.target.value);
+  };
   const handleRead = () => {
-    window.backend.Drill.Open("COM" + port.toString()).then((result)=>{
-      setIsConnected(true)
-      enqueueSnackbar("Drill Connected", successSnackBarOptions)
-      window.backend.Drill.GetInfo().then((result) => {
-        setInfo(result)
-        setCurrentOffset(result.CurrentOffset)
-        window.backend.Drill.GetProfile().then((result) => {
-          const newProfile = mapFieldsToProfile(result.Fields);
-          setProfile(newProfile);
-          setIsConnected(true)
-        });
-      }).catch((err)=>{
-        console.log(err)
-        enqueueSnackbar("Error getting info" + err, errorSnackBarOptions);
-        setIsConnected(false)
+    window.backend.Drill.Open("COM" + port.toString())
+      .then((result) => {
+        setIsConnected(true);
+        enqueueSnackbar("Drill Connected", successSnackBarOptions);
+        window.backend.Drill.GetInfo()
+          .then((result) => {
+            setInfo(result);
+            setCurrentOffset(result.CurrentOffset);
+            window.backend.Drill.GetProfile().then((result) => {
+              const newProfile = mapFieldsToProfile(result.Fields);
+              setProfile(newProfile);
+              setIsConnected(true);
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            enqueueSnackbar("Error getting info" + err, errorSnackBarOptions);
+            setIsConnected(false);
+          });
+      })
+      .catch((err) => {
+        enqueueSnackbar("Error connecting: " + err, errorSnackBarOptions);
+        setIsConnected(false);
       });
-    }).catch((err)=>{
-      enqueueSnackbar("Error connecting: "+err, errorSnackBarOptions);
-      setIsConnected(false)
-    })
-    
-
   };
 
   // const handleWrite = () => {
@@ -109,32 +123,34 @@ export default function HelloWorld() {
           const data = [...profile];
           data[data.indexOf(oldData)] = cleanFormat(newData);
           setProfile(data);
-          window.backend.Drill.WriteParam(cleanFormat(newData)).then(
-            (result) => {
+          window.backend.Drill.WriteParam(cleanFormat(newData))
+            .then((result) => {
               enqueueSnackbar("Parameter Saved", successSnackBarOptions);
-            }
-          ).catch((err)=>{
-            enqueueSnackbar("Error saving:" + err, errorSnackBarOptions);
-          });
+            })
+            .catch((err) => {
+              enqueueSnackbar("Error saving:" + err, errorSnackBarOptions);
+            });
         }
       }, 600);
     });
 
   return (
     <div className="App">
-      <Grid container spacing={3}>
-        <Grid item xs={2} style={{ minWidth: "300px" }}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <InfoCard isConnected={isConnected} data={info} currentOffset={currentOffset} handleOpen={handleRead} handleClose={handleClose} handleSetPort={handleSetPort}/>
-            </Grid>
-          </Grid>
+      <Grid container spacing={1}>
+        <Grid item>
+          <InfoCard
+            isConnected={isConnected}
+            data={info}
+            currentOffset={currentOffset}
+            handleOpen={handleRead}
+            handleClose={handleClose}
+            handleSetPort={handleSetPort}
+          />
         </Grid>
         <Grid
           item
-          xs={4}
           style={{
-            minWidth: "400px",
+            minWidth: "457px",
           }}
         >
           <ParamTable
@@ -146,9 +162,8 @@ export default function HelloWorld() {
         </Grid>
         <Grid
           item
-          xs={4}
           style={{
-            minWidth: "400px",
+            minWidth: "457px",
           }}
         >
           <ParamTable
